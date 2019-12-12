78be6ff80800e74bc883be9bf6291d100f11ab33
Shivam Garg <shivgarg@live.com>
2019-12-11 21:09:38 -0600

    modifying run.sh batch size parameter

--- examples/run.sh
+++ examples/run.sh
@@ -1,17 +1,17 @@
 python run_lm_finetuning.py \
-            --output_dir=gpt_cose_multi_task \
-            --model_type=openai-gpt \
-            --model_name_or_path=openai-gpt \
+            --output_dir=gpt2_cose_multi_task \
+            --model_type=gpt2 \
+            --model_name_or_path=gpt2 \
             --do_train \
             --evaluate_during_training \
             --train_data_file=$1 \
             --do_eval \
             --eval_data_file=$2 \
-            --per_gpu_train_batch_size=2 \
-            --gradient_accumulation_steps=4 \
+            --per_gpu_train_batch_size=1 \
+            --gradient_accumulation_steps=8 \
             --per_gpu_eval_batch_size=1 \
             --warmup_steps=600 \
             --num_train_epochs=4 \
-            --block_size=128 \
+            --block_size=192 \
 	    --do_lower_case \
             --overwrite_cache
