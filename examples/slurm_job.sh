15445cf85dfa7dfbb6097b276e140bcbf373e5ec
Shivam Garg <shivgarg@live.com>
2019-12-11 22:03:19 -0600

    adding slurm script

---
+++ examples/slurm_job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -J gpt2_cose_multi_options             # Job name
+#SBATCH -o /work/07010/sgarg/maverick2/nlp_project/transformers/examples/training_logs/gpt2-cose-multi_options.out       # Specify stdout output file (%j expands to jobId)
+#SBATCH -e /work/07010/sgarg/maverick2/nlp_project/transformers/examples/training_logs/gpt2-cose-multi_options.err       # stderr; skip to combine stdout and stderr
+#SBATCH -p gtx                   # Queue name
+#SBATCH -N 1                     # Total number of nodes requested (16 cores/node)
+#SBATCH -n 1                     # Total number of tasks
+#SBATCH -t 24:00:00              # Run time (hh:mm:ss) - 1.5 hours
+
+#SBATCH -A RL-grad-class                # Specify allocation to charge against
+# Slurm email notifications are now working on Lonestar 5 
+#SBATCH --mail-user=shivamgarg@utexas.edu
+#SBATCH --mail-type=begin   # email me when the job starts
+#SBATCH --mail-type=end     # email me when the job finishes
+# Load any necessary modules (these are examples)
+# Loading modules in the script ensures a consistent environment.
+module load intel
+module load python3/3.7.0
+module load cuda cudnn
+
+# Launch the executable named "a.out"
+source /work/07010/sgarg/maverick2/venv/bin/activate
+sh run.sh $1 $2
