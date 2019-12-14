873d91d39656a99e4e94d4a285f2c6cff85ef5f1
Shivam Garg <shivgarg@live.com>
2019-12-13 20:12:58 -0600

    tacc scripts

---
+++ examples/slurm_job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -J gpt_code_multi-task_train         # Job name
+#SBATCH -o /work/07010/sgarg/maverick2/nlp_project/transformers/examples/training_logs/gpt-cose-multi_train.out       # Specify stdout output file (%j expands to jobId)
+#SBATCH -p gtx                   # Queue name
+#SBATCH -N 1                     # Total number of nodes requested (16 cores/node)
+#SBATCH -n 1                     # Total number of tasks
+#SBATCH -t 24:00:00              # Run time (hh:mm:ss) - 1.5 hours
+
+#SBATCH -A RL-grad-class                # Specify allocation to charge against
+# Slurm email notifications are now working on Lonestar 5 
+#SBATCH --mail-user=shivamgarg@utexas.edu
+#SBATCH --mail-type=begin   # email me when the job starts
+#SBATCH --mail-type=end     # email me when the job finishes
+# Load any necessary modules (these are examples)
+# Loading modules in the script ensures a consistent environment.
+module load intel
+module load python3/3.7.0
+module load cuda cudnn
+
+# Launch the executable named "a.out"
+source /work/07010/sgarg/maverick2/venv/bin/activate
+sh run_gen.sh $1 $2 $3 $4
+
+
